4ef7fe4ba70e1102eba2cf431674ed719fae7ad4
moxuse <moxuse@gmail.com>
2017-02-01 15:52:53 +0900

    Movieの位置計算再開

--- src/ofxAEMovieCap.cpp
+++ src/ofxAEMovieCap.cpp
@@ -28,6 +28,7 @@ void MovieCap::loadMovie(const string& filepath)
 {
     #ifdef TARGET_LINUX
         movie_.setPlayer(ofPtr<ofBaseVideoPlayer>(new ofGstVideoPlayer));
+        ofLog(OF_LOG_NOTICE, "chosen ofGstVideoPlayer");
     #endif
 	movie_.setPixelFormat(OF_PIXELS_RGBA);
     movie_.load(filepath);
@@ -38,10 +39,10 @@ void MovieCap::loadMovie(const string& filepath)
 void MovieCap::setActive(bool active)
 {
 	active = use_audio_&&active;
-//	if(active != movie_.isPlaying()) {
-//		active?movie_.play():movie_.stop();
-//	}
-    movie_.play();
+	if(active != movie_.isPlaying()) {
+		active?movie_.play():movie_.stop();
+	}
+    //movie_.play();
 }
 
 void MovieCap::setUseAudio(bool use_audio)
@@ -53,20 +54,20 @@ void MovieCap::setUseAudio(bool use_audio)
 void MovieCap::update()
 {
 	if(comp_) {
-//		float movie_position, app_position;
-//		if(comp_->isBackward()) {
-//			movie_.setSpeed(-comp_->getSpeed());
-//			movie_position = movie_.getPosition()*movie_.getDuration();
-//			app_position = movie_.getDuration()-layer_->getFrame()/comp_->getFrameRate();
-//		}
-//		else {
-//			movie_.setSpeed(comp_->getSpeed());
-//			movie_position = movie_.getPosition()*movie_.getDuration();
-//			app_position = layer_->getFrame()/comp_->getFrameRate();
-//		}
-//		if(!use_audio_ || abs(app_position-movie_position) > abs(comp_->getSpeed()*lazy_seconds_)) {
-//			movie_.setPosition(app_position/movie_.getDuration());
-//		}
+		float movie_position, app_position;
+		if(comp_->isBackward()) {
+			movie_.setSpeed(-comp_->getSpeed());
+			movie_position = movie_.getPosition()*movie_.getDuration();
+			app_position = movie_.getDuration()-layer_->getFrame()/comp_->getFrameRate();
+		}
+		else {
+			movie_.setSpeed(comp_->getSpeed());
+			movie_position = movie_.getPosition()*movie_.getDuration();
+			app_position = layer_->getFrame()/comp_->getFrameRate();
+		}
+		if(!use_audio_ || abs(app_position-movie_position) > abs(comp_->getSpeed()*lazy_seconds_)) {
+			movie_.setPosition(app_position/movie_.getDuration());
+		}
 	}
 	movie_.update();
 }
